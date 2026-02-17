b508168f8882a08cccc3c9cf65b41319dca95859
Milad Lashini <milad.lashini@gmail.com>
2026-02-18 00:19:47 +0100

    Added Tabby.

--- nvim/init.lua
+++ nvim/init.lua
@@ -390,7 +390,65 @@ require("lazy").setup({
                highlight DashboardHeader guifg=#66FFFF
                ]])
         end,
-        },
+    },
+
+    --------------------------------------------------
+    -- Tabby
+    --------------------------------------------------
+    {
+    "nanozuki/tabby.nvim",
+        config = function()
+            local theme = {
+            fill = 'TabLineFill',
+            -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+            head = 'TabLine',
+            current_tab = 'TabLineSel',
+            tab = 'TabLine',
+            win = 'TabLine',
+            tail = 'TabLine',
+            }
+            require('tabby').setup({
+            line = function(line)
+                return {
+                {
+                    { '  ', hl = theme.head },
+                    line.sep('', theme.head, theme.fill),
+                },
+                line.tabs().foreach(function(tab)
+                    local hl = tab.is_current() and theme.current_tab or theme.tab
+                    return {
+                    line.sep('', hl, theme.fill),
+                    tab.is_current() and '' or '󰆣',
+                    tab.number(),
+                    tab.name(),
+                    tab.close_btn(''),
+                    line.sep('', hl, theme.fill),
+                    hl = hl,
+                    margin = ' ',
+                    }
+                end),
+                line.spacer(),
+                line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+                    return {
+                    line.sep('', theme.win, theme.fill),
+                    win.is_current() and '' or '',
+                    win.buf_name(),
+                    line.sep('', theme.win, theme.fill),
+                    hl = theme.win,
+                    margin = ' ',
+                    }
+                end),
+                {
+                    line.sep('', theme.tail, theme.fill),
+                    { '  ', hl = theme.tail },
+                },
+                hl = theme.fill,
+                }
+            end,
+            -- option = {}, -- setup modules' option,
+            })
+        end,
+    }
 
 })
 
@@ -418,5 +476,31 @@ vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#3c3836" })
 
 vim.keymap.set("n", "<leader>ps", "<cmd>Store<cr>", { desc = "Open Plugin Store" })
+
+--------------------------------------------------
+-- Tab Navigation
+--------------------------------------------------
+
+-- New tab
+vim.keymap.set('n', '<leader>tn', '<cmd>tabnew<CR>', { desc = 'New Tab' })
+
+-- Close current tab
+vim.keymap.set('n', '<leader>tc', '<cmd>tabclose<CR>', { desc = 'Close Tab' })
+
+-- Next / Previous tab
+vim.keymap.set('n', '<S-Left>', '<cmd>tabnext<CR>', { desc = 'Next Tab' })
+vim.keymap.set('n', '<S-Right>', '<cmd>tabprevious<CR>', { desc = 'Previous Tab' })
+
+-- Go to tab by number
+for i = 1, 9 do
+  vim.keymap.set('n', '<leader>' .. i, i .. 'gt', { desc = 'Go to Tab ' .. i })
+end
+
+-- Move tab left / right
+vim.keymap.set('n', '<leader>tmh', '<cmd>-tabmove<CR>', { desc = 'Move Tab Left' })
+vim.keymap.set('n', '<leader>tml', '<cmd>+tabmove<CR>', { desc = 'Move Tab Right' })
+
+-- Close all tabs except current
+vim.keymap.set('n', '<leader>to', '<cmd>tabonly<CR>', { desc = 'Close Other Tabs' })
 
 
