a947e2e7aebb04e576c92269f802f8a13fbd5c5c
Milad Lashini <milad.lashini@gmail.com>
2026-02-22 22:11:35 +0100

    remive the font zip file.

--- install.sh
+++ install.sh
@@ -311,6 +311,7 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
 unzip JetBrainsMono.zip
+rm JetBrainsMono.zip
 fc-cache -fv
 
 
