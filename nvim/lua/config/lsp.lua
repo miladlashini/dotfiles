69773f900e6475181cbf88676c68d70019d249dc
Milad Lashini <milad.lashini@gmail.com>
2026-03-26 15:28:26 +0000

    Activated go to declration to lsp.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -17,6 +17,7 @@ function M.setup()
         local opts = { buffer = bufnr }
 
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
